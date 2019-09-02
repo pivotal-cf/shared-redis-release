f78051d54dc9deb4756a22e13c8b7e39d9d1f3db
Mirah Gary <mgary@pivotal.io>
2019-09-02 14:07:09 +0100

    Bump ginkgo and update script to compile ginkgo.
    [#167920648]
    
    Signed-off-by: James Norman <jnorman@pivotal.io>

--- scripts/compile_ginkgo.sh
+++ scripts/compile_ginkgo.sh
@@ -5,20 +5,12 @@ set -eu
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 PROJECT_DIR="$( dirname "$DIR" )"
 
-GOPATH="$( mktemp -d )"
+GOPATH="$(mktemp -d)"
 mkdir "${GOPATH}/src"
 cp -R "${PROJECT_DIR}/src/cf-redis-smoke-tests/vendor/" "${GOPATH}/src/"
 
-go_version="$(go version)"
-ginkgo_sha="$( jq --raw-output '.dependencies | map(select(.importpath == "github.com/onsi/ginkgo")) | .[0].revision' "${PROJECT_DIR}/src/cf-redis-smoke-tests/vendor/manifest" )"
-
-pushd "$GOPATH"
-  GOOS=linux GOARCH=amd64 go build github.com/onsi/ginkgo/ginkgo
+pushd "${GOPATH}/src/github.com/onsi/ginkgo"
+  GOOS=linux GOARCH=amd64 go build -o "${PROJECT_DIR}/ginkgo"
 popd
 
-cp "${GOPATH}/ginkgo" "${PROJECT_DIR}/"
-rm -rf "$GOPATH"
-
 echo "compiled ginkgo successfully for linux amd64"
-echo "$go_version"
-echo "ginkgo git sha: ${ginkgo_sha}"
