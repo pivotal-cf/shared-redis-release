c0f4736ac85664c217aa0b89b3e3515e2066fae8
Mirah Gary <mgary@pivotal.io>
2019-09-03 11:50:22 +0100

    Compile ginkgo correctly.
    [#167920648]
    
    Signed-off-by: James Norman <jnorman@pivotal.io>

--- scripts/compile_ginkgo.sh
+++ scripts/compile_ginkgo.sh
@@ -5,12 +5,9 @@ set -eu
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 PROJECT_DIR="$( dirname "$DIR" )"
 
-GOPATH="$(mktemp -d)"
-mkdir "${GOPATH}/src"
-cp -R "${PROJECT_DIR}/src/cf-redis-smoke-tests/vendor/" "${GOPATH}/src/"
-
-pushd "${GOPATH}/src/github.com/onsi/ginkgo"
-  GOOS=linux GOARCH=amd64 go build -o "${PROJECT_DIR}/ginkgo"
+export GOPATH=$HOME/go
+pushd "$GOPATH"
+  GOOS=linux GOARCH=amd64 go build -o "${PROJECT_DIR}/ginkgo" "github.com/onsi/ginkgo/ginkgo"
 popd
 
 echo "compiled ginkgo successfully for linux amd64"
