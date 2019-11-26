599705c2a639a8ebb2ad863675f983265b667a6b
Mirah Gary <mgary@pivotal.io>
2019-11-26 10:41:36 +0100

    Simplify shared-redis packaging to work with auto-bumping.
    [#163790583]
    
    Signed-off-by: James Norman <jnorman@pivotal.io>

--- spec/system/security_spec.rb
+++ spec/system/security_spec.rb
@@ -6,11 +6,6 @@ require 'timeout'
 
 describe 'security' do
   describe 'the broker' do
-    it 'uses latest version of nginx' do
-      output = bosh.ssh(deployment_name, Helpers::Environment::BROKER_INSTANCE_NAME, '/var/vcap/packages/cf-redis-nginx/sbin/nginx -v')
-      expect(output).to eql('nginx version: nginx/1.16.1')
-    end
-
     it 'does not listen publicly on the backend_port' do
       netstat_output = bosh.ssh(deployment_name, Helpers::Environment::BROKER_INSTANCE_NAME, "netstat -l | grep #{broker_backend_port}")
       expect(netstat_output.lines.count).to eq(1)
