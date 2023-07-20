5ca1da839064d49d6d35affc64c45cbc110ea1c2
Julian Benitez Gutierrez <jubenitez@vmware.com>
2023-07-19 19:37:00 -0500

    Use netstat full ip output

--- spec/system/security_spec.rb
+++ spec/system/security_spec.rb
@@ -7,9 +7,10 @@ require 'timeout'
 describe 'security' do
   describe 'the broker' do
     it 'does not listen publicly on the backend_port' do
-      netstat_output = bosh.ssh(deployment_name, Helpers::Environment::BROKER_INSTANCE_NAME, "netstat -l | grep #{broker_backend_port}")
+      netstat_output = bosh.ssh(deployment_name, Helpers::Environment::BROKER_INSTANCE_NAME, "netstat -W -l | grep #{broker_backend_port}")
+      hostname = bosh.ssh(deployment_name, Helpers::Environment::BROKER_INSTANCE_NAME, "hostname")
       expect(netstat_output.lines.count).to eq(1)
-      expect(netstat_output).to include("localhost:#{broker_backend_port}")
+      expect(netstat_output).to match(/(#{hostname}|localhost):#{broker_backend_port}/)
     end
   end
 end
